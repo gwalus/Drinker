d2e43584367819a1471f54c04c761c51dd95faac
Grzegorz Waluś <gwalus1404@gmail.com>
2021-03-29 09:50:40 +0200

    Implementation GetDrinkByIdAsync functionality in DataService

--- DataDownloader/Interfaces/IDataService.cs
+++ DataDownloader/Interfaces/IDataService.cs
@@ -3,9 +3,21 @@ using System.Threading.Tasks;
 
 namespace DataDownloader.Interfaces
 {
+    /// <summary>
+    /// Interface contains methods to operations on data.
+    /// </summary>
     public interface IDataService
     {
-        Task<CoctailDbResponseModel> GetDrink(int id);
-        Task<DrinkBsonItem> SaveDrink();
+        /// <summary>
+        /// Getting one element from coctailsDB.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>DrinkItem</returns>
+        Task<DrinkItem> GetDrinkByIdAsync(int id);
+        /// <summary>
+        /// Save drink to the database.
+        /// </summary>
+        /// <returns></returns>
+        Task SaveDrink(DrinkBsonItem item);
     }
 }
---
+++ DataDownloader/Services/DataService.cs
@@ -0,0 +1,40 @@
+﻿using DataDownloader.Interfaces;
+using DataDownloader.Models;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DataDownloader.Services
+{
+    ///<summary>
+    /// Implementation of the interface IDataService
+    /// </summary>
+    ///<inheritdoc/>    
+    public class DataService : IDataService
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<DrinkItem> GetDrinkByIdAsync(int id)
+        {
+            var request = await _httpClient.GetAsync($"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={id}");
+
+            if (request.Content == null) 
+                return null;
+
+            var response = await request.Content.ReadAsStringAsync();
+
+            var drink = JsonSerializer.Deserialize<CoctailDbResponseModel>(response)
+                ?.drinks
+                ?.FirstOrDefault();
+
+            return drink;
+        }
+
+        public Task SaveDrink(DrinkBsonItem item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
