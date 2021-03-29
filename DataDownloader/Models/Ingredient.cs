23751a6daa6c29617510d6780feab19160a4098a
Grzegorz Waluś <gwalus1404@gmail.com>
2021-03-29 09:54:25 +0200

    Add Ingredient model class

---
+++ DataDownloader/Models/Ingredient.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace DataDownloader.Models
+{
+    /// <summary>
+    /// Drink ingredient model.
+    /// </summary>
+    public class Ingredient
+    {
+        [BsonElement]
+        public string Name { get; set; }
+        [BsonElement]
+        public string Measure { get; set; }
+    }
+}
