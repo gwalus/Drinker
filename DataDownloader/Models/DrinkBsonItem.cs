fefe0e1d9e9377c494a2dd96d900a210956a29ad
Grzegorz Waluś <gwalus1404@gmail.com>
2021-03-29 09:25:38 +0200

    Add DrinkBsonItem mongodb document model

---
+++ DataDownloader/Models/DrinkBsonItem.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace DataDownloader.Models
+{
+    /// <summary>
+    /// A class representing the mongodb document model.
+    /// </summary>
+    public class DrinkBsonItem
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+        public string DrinkId { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Alcoholic { get; set; }
+        public string Glass { get; set; }
+        public string Instructions { get; set; }
+        public string PhotoUrl { get; set; }
+        public string DateModified { get; set; }
+        public ICollection<BsonDocument> Ingradients { get; set; }
+    }
+}
