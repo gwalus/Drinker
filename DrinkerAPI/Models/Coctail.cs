d31a3476bbb32df98420d9abef6a696520c2090d
Grzegorz Waluś <gwalus1404@gmail.com>
2021-04-11 12:54:26 +0200

    Add Coctail model

---
+++ DrinkerAPI/Models/Coctail.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace DrinkerAPI.Models
+{
+    public class Coctail
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Alcoholic { get; set; }
+        public string Glass { get; set; }
+        public string Instructions { get; set; }
+        public string PhotoUrl { get; set; }
+        public string DateModified { get; set; }
+        public ICollection<Ingredient> Ingradients { get; set; }
+    }
+}
