e458d5a1d8c4243976992f89a0a043492faa0f7d
Grzegorz Waluś <gwalus1404@gmail.com>
2021-04-11 14:01:59 +0200

    Add CreateIngredients method

--- DataDownloader/Helpers/DataProvider.cs
+++ DataDownloader/Helpers/DataProvider.cs
@@ -1,4 +1,6 @@
 ﻿using DataDownloader.Models;
+using DrinkerAPI.Models;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
@@ -25,6 +27,60 @@ namespace DataDownloader.Helpers
             if (drink == null) return null;
 
             return drink;
+        }
+
+        public static IList<Ingredient> CreateIngredients(DrinkItem drink)
+        {
+            IList<string> ingradientNames = new List<string>();
+            IList<string> ingradientMeasures = new List<string>();
+
+            ingradientNames.Add(drink.strIngredient1);
+            ingradientNames.Add(drink.strIngredient2);
+            ingradientNames.Add(drink.strIngredient3);
+            ingradientNames.Add(drink.strIngredient4);
+            ingradientNames.Add(drink.strIngredient5);
+            ingradientNames.Add(drink.strIngredient6);
+            ingradientNames.Add(drink.strIngredient7);
+            ingradientNames.Add(drink.strIngredient8);
+            ingradientNames.Add(drink.strIngredient9);
+            ingradientNames.Add(drink.strIngredient10);
+            ingradientNames.Add(drink.strIngredient11);
+            ingradientNames.Add(drink.strIngredient12);
+            ingradientNames.Add(drink.strIngredient13);
+
+            ingradientNames = ingradientNames.Where(ing => ing != null).ToList();
+
+            ingradientMeasures.Add(drink.strMeasure1);
+            ingradientMeasures.Add(drink.strMeasure2);
+            ingradientMeasures.Add(drink.strMeasure3);
+            ingradientMeasures.Add(drink.strMeasure4);
+            ingradientMeasures.Add(drink.strMeasure5);
+            ingradientMeasures.Add(drink.strMeasure6);
+            ingradientMeasures.Add(drink.strMeasure7);
+            ingradientMeasures.Add(drink.strMeasure8);
+            ingradientMeasures.Add(drink.strMeasure9);
+            ingradientMeasures.Add(drink.strMeasure10);
+            ingradientMeasures.Add(drink.strMeasure11);
+            ingradientMeasures.Add(drink.strMeasure12);
+            ingradientMeasures.Add(drink.strMeasure13);
+
+            ingradientMeasures = ingradientMeasures.Where(mea => mea != null).ToList();
+
+            IList<Ingredient> ingredients = new List<Ingredient>();
+
+            for (int i = 0; i < ingradientNames.Count; i++)
+            {
+                var ingredient = new Ingredient
+                {
+                    Id = i,
+                    Name = ingradientNames.ElementAtOrDefault(i),
+                    Measure = ingradientMeasures.ElementAtOrDefault(i)?.TrimEnd()
+                };
+
+                ingredients.Add(ingredient);
+            }
+
+            return ingredients;
         }
     }
 }
