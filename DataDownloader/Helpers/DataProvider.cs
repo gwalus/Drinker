83f00ece9a7e8d2ec4f67cdeb5da23953a17ec58
Grzegorz Waluś <gwalus1404@gmail.com>
2021-04-11 14:00:35 +0200

    Add GetDrinkById from api method

---
+++ DataDownloader/Helpers/DataProvider.cs
@@ -0,0 +1,30 @@
+﻿using DataDownloader.Models;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DataDownloader.Helpers
+{
+    public class DataProvider
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<DrinkItem> GetDrinkByIdAsync(int id)
+        {
+            var request = await _httpClient.GetAsync($"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={id}");
+
+            if (request.Content == null) return null;
+
+            var response = await request.Content.ReadAsStringAsync();
+
+            var drink = JsonSerializer.Deserialize<CoctailResponseModel>(response)
+                ?.drinks
+                ?.FirstOrDefault();
+
+            if (drink == null) return null;
+
+            return drink;
+        }
+    }
+}
