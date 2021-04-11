b58f9ad423a98dc030a8bf1f40f45712270a6f6d
Jakub Gawęda <kubag98@hotmail.com>
2021-04-11 17:17:35 +0200

    Add ICoctailRepository with method GetListOfCoctails

---
+++ DrinkerAPI/Controllers/CoctailController.cs
@@ -0,0 +1,20 @@
+﻿using DrinkerAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkerAPI.Controllers
+{
+    public class CoctailController : BaseApiController
+    {
+        //[HttpGet("list")]
+        //async Task<List<Coctail>> ListOfProducts()
+        //{
+
+        //}
+   
+  
+    }
+}
---
+++ DrinkerAPI/Interfaces/ICoctailRepository.cs
@@ -0,0 +1,13 @@
+﻿using DrinkerAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkerAPI.Interfaces
+{
+    interface ICoctailRepository
+    {
+        Task<List<Coctail>> GetListOfCoctails();
+    }
+}
