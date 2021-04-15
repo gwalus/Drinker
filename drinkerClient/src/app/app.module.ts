dca798e443db5496ead2285089a448494372a85d
Sławomir Kędzior <matikm18@gmail.com>
2021-04-15 16:46:52 +0200

    add coctail-builder and design header component

--- drinkerClient/src/app/app.module.ts
+++ drinkerClient/src/app/app.module.ts
@@ -5,17 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
-import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
+import { CoctailBuilderComponent } from './content/coctail-builder/coctail-builder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ContentComponent,
-    FooterComponent
+    FooterComponent,
+    CoctailBuilderComponent
   ],
   imports: [
     BrowserModule,
--- drinkerClient/src/app/content/coctail-builder/coctail-builder.component.spec.ts
+++ drinkerClient/src/app/content/coctail-builder/coctail-builder.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ContentComponent } from './content.component';
+import { CoctailBuilderComponent } from './coctail-builder.component';
 
-describe('ContentComponent', () => {
-  let component: ContentComponent;
-  let fixture: ComponentFixture<ContentComponent>;
+describe('CoctailBuilderComponent', () => {
+  let component: CoctailBuilderComponent;
+  let fixture: ComponentFixture<CoctailBuilderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContentComponent ]
+      declarations: [ CoctailBuilderComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ContentComponent);
+    fixture = TestBed.createComponent(CoctailBuilderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
