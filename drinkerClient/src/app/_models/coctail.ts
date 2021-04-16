0bf68815c34ebd35fbc99f79b0bb3ea73caa45b7
Grzegorz Waluś <gwalus1404@gmail.com>
2021-04-16 14:23:35 +0200

    Add coctail model in client

---
+++ drinkerClient/src/app/_models/coctail.ts
@@ -0,0 +1,13 @@
+import { Ingredient } from "./ingredient";
+
+export interface Coctail {
+    id: number;
+    name: string;
+    category: string;
+    alcoholic: string;
+    glass: string;
+    instructions: string;
+    photoUrl: string;
+    dateModified: Date,
+    ingredients: Ingredient[]
+}
