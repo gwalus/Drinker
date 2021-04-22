3c109f843128c28c8afa5a15124326f89ce1e1ab
Grzegorz Waluś <gwalus1404@gmail.com>
2021-04-22 12:51:51 +0200

    Add PaginationParams class

---
+++ DrinkerAPI/Helpers/PaginationParams.cs
@@ -0,0 +1,15 @@
+﻿namespace DrinkerAPI.Helpers
+{
+    public class PaginationParams
+    {
+        private const int _maxPageSize = 24;
+        public int PageNumber { get; set; } = 1;
+        private int _pageSize = 8;
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = (value > _maxPageSize) ? _maxPageSize : value;
+        }
+    }
+}
