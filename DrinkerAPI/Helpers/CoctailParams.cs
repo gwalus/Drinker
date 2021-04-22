b89a4c555c392c830ad1a8c425d2de5a1750e47a
Grzegorz Waluś <gwalus1404@gmail.com>
2021-04-22 13:00:33 +0200

    Add CoctailParams class

---
+++ DrinkerAPI/Helpers/CoctailParams.cs
@@ -0,0 +1,9 @@
+﻿namespace DrinkerAPI.Helpers
+{
+    public class CoctailParams : PaginationParams
+    {
+        public string Category { get; set; } = "Ordinary Drink";
+        public string Alcoholic { get; set; } = "Alcoholic";
+        public string Glass { get; set; }
+    }
+}
