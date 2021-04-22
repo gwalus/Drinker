77fe885e5ae8c3a0e869e9bfdcaf095bb56b5926
Grzegorz Waluś <gwalus1404@gmail.com>
2021-04-22 13:51:41 +0200

    Add pagination header to response

--- DrinkerAPI/Controllers/CoctailController.cs
+++ DrinkerAPI/Controllers/CoctailController.cs
@@ -1,4 +1,5 @@
-﻿using DrinkerAPI.Helpers;
+﻿using DrinkerAPI.Extensions;
+using DrinkerAPI.Helpers;
 using DrinkerAPI.Interfaces;
 using DrinkerAPI.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -29,15 +30,38 @@ namespace DrinkerAPI.Controllers
 
             return BadRequest("Unable to connect...");
         }
+
+
+        /// <summary>Gets the coctails by ingredient.</summary>
+        /// <param name="ingredients">The ingredients.</param>
+        /// <param name="coctailParams">The coctail parameters.</param>
+        /// <returns>
+        ///   <br />
+        /// </returns>
         [HttpGet("search/byIngredients")]
         public async Task<ActionResult<IList<Coctail>>> GetCoctailsByIngredient([FromQuery] IList<string> ingredients, [FromQuery] CoctailParams coctailParams)
         {
             if (ingredients.Count == 0) 
                 return BadRequest("Please to add minimum one ingredient");
 
-            return Ok(await _coctailRepository.GetCoctailsByIngredientsAsync(ingredients, coctailParams));
+            var coctail = await _coctailRepository.GetCoctailsByIngredientsAsync(ingredients, coctailParams);
 
+            if(coctail != null)
+            {
+                Response.AddPaginationHeader(coctail.CurrentPage, coctail.PageSize, coctail.TotalCount, coctail.TotalPages);
+                
+                return Ok(coctail);
+            }
+
+            return NotFound("No cocktails found with the specified filters.");
         }
+
+
+        /// <summary>Gets the coctail by name.</summary>
+        /// <param name="keyword">The keyword.</param>
+        /// <returns>
+        ///   <br />
+        /// </returns>
         [HttpGet("search/byName/{keyword}")]
         public async Task<ActionResult<Coctail>> GetCoctailByName(string keyword)
         {
