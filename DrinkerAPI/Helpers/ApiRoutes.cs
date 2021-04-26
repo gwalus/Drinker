8ddb5a23c401cc06dbe0c5b820b014f330e915c7
Jakub Gawęda <kubag98@hotmail.com>
2021-04-26 17:48:35 +0200

    Use new ApiRouts

--- DrinkerAPI/Helpers/ApiRoutes.cs
+++ DrinkerAPI/Helpers/ApiRoutes.cs
@@ -7,5 +7,14 @@ namespace DrinkerAPI.Helpers
 {
     public class ApiRoutes
     {
+        public const string Root = "api";
+        public const string Version = "v1";
+        public const string Base = Root + "/" + Version;
+        public static class Coctails
+        {
+            public const string ListAll = Base + "/coctails";
+            public const string ByIngredients = Base + "/Ingredients";
+            public const string ByName = Base + "/{keyword}";
+        }
     }
 }
