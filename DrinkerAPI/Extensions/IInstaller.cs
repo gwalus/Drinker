8f33fc9e2ca5979783f83653cf9ae13a8f798007
Jakub Gawęda <kubag98@hotmail.com>
2021-04-26 17:50:27 +0200

    Add IInstaller with InstallServices method

---
+++ DrinkerAPI/Extensions/IInstaller.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace DrinkerAPI.Extensions
+{
+    interface IInstaller
+    {
+        void InstallServices(IServiceCollection services, IConfiguration configuration);
+    }
+}
