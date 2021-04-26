1c1add095663b4d969bc8d7ab51c897b2a64e8ee
Sławomir Kędzior <matikm18@gmail.com>
2021-04-26 12:55:29 +0200

    add login component

--- drinkerClient/src/app/app.module.ts
+++ drinkerClient/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { GameComponent } from './content/game/game.component';
 import { CalculatorComponent } from './content/calculator/calculator.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 
 
@@ -25,7 +26,8 @@ import { FormsModule } from '@angular/forms';
     CoctailBuilderComponent,
     DrinksComponent,
     GameComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
