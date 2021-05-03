812ded62cf710e41860e59b6857c48b453fa052a
Jakub Gawęda <kubag98@hotmail.com>
2021-05-03 19:23:51 +0200

    Add LoginAsync

--- DrinkerAPI/Interfaces/IIdentityService.cs
+++ DrinkerAPI/Interfaces/IIdentityService.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using DrinkerAPI.Models;
 using System.Threading.Tasks;
 
 namespace DrinkerAPI.Interfaces
 {
     interface IIdentityService
     {
+        Task<AuthentiactionResult> RegisterAsync(string email, string password);
+        Task<AuthentiactionResult> LoginAsync(string email, string password);
     }
 }
