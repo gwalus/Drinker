cc227c04c731844267d47e6bfff78766b5f68d37
Jakub Gawęda <kubag98@hotmail.com>
2021-05-03 19:26:44 +0200

    Add GenerateAuthenticationResultForUser in IndentityService

--- DrinkerAPI/Services/IdentityService.cs
+++ DrinkerAPI/Services/IdentityService.cs
@@ -2,9 +2,13 @@
 using DrinkerAPI.Models;
 using DrinkerAPI.Options;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace DrinkerAPI.Services
@@ -26,6 +30,29 @@ namespace DrinkerAPI.Services
         public Task<AuthentiactionResult> RegisterAsync(string email, string password)
         {
             throw new NotImplementedException();
+        }
+        private AuthentiactionResult GenerateAuthenticationResultForUser(IdentityUser newUser)
+        {
+            var tokenHanlder = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, newUser.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(JwtRegisteredClaimNames.Email, newUser.Email),
+                    new Claim("id", newUser.Id)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHanlder.CreateToken(tokenDescriptor);
+            return new AuthentiactionResult
+            {
+                Success = true,
+                Token = tokenHanlder.WriteToken(token)
+            };
         }
     }
 }
