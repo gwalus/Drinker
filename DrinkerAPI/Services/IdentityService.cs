cbda2aa6849fce61f6713506096d5676ae6bcf84
Jakub Gawęda <kubag98@hotmail.com>
2021-05-03 19:25:15 +0200

    Add IdentityService

---
+++ DrinkerAPI/Services/IdentityService.cs
@@ -0,0 +1,31 @@
+﻿using DrinkerAPI.Interfaces;
+using DrinkerAPI.Models;
+using DrinkerAPI.Options;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkerAPI.Services
+{
+    public class IdentityService : IIdentityService
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly JwtSettings _jwtSettings;
+        public IdentityService(UserManager<IdentityUser> userManager, JwtSettings jwtSettings)
+        {
+            _userManager = userManager;
+            _jwtSettings = jwtSettings;
+        }
+        public Task<AuthentiactionResult> LoginAsync(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AuthentiactionResult> RegisterAsync(string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
