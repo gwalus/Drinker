35ddf9f5f360c17e4660bc8967d0412770dc8889
Jakub Gawęda <kubag98@hotmail.com>
2021-05-03 19:36:35 +0200

    Implement login endpoint in IdentityController

--- DrinkerAPI/Controllers/IdentityController.cs
+++ DrinkerAPI/Controllers/IdentityController.cs
@@ -1,9 +1,13 @@
-﻿using DrinkerAPI.Interfaces;
+﻿using DrinkerAPI.Helpers;
+using DrinkerAPI.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TweetBook.Contract.v1.Requests;
+using TweetBook.Contract.v1.Responses;
+using TweetBook.Controllers.v1;
 
 namespace DrinkerAPI.Controllers
 {
@@ -18,9 +22,26 @@ namespace DrinkerAPI.Controllers
         {
             return View();
         }
+        [HttpPost(ApiRoutes.Identity.Register)]
         public async Task<ActionResult> Register([FromBody] UserRegistrationRequest request)
         {
             var authResponse = await _identityService.RegisterAsync(request.Email, request.Password);
+            if (!authResponse.Success)
+            {
+                return BadRequest(new AuthFailedResponse
+                {
+                    Errors = authResponse.Errors
+                });
+            }
+            return Ok(new AuthSuccessResponse
+            {
+                Token = authResponse.Token
+            });
+        }
+        [HttpPost(ApiRoutes.Identity.Login)]
+        public async Task<ActionResult> Login([FromBody] UserLoginRequest request)
+        {
+            var authResponse = await _identityService.LoginAsync(request.Email, request.Password);
             if (!authResponse.Success)
             {
                 return BadRequest(new AuthFailedResponse
