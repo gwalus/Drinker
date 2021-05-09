9707409a9eb531c9c91c6394201f6eb2c9e31efd
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-09 20:48:06 +0200

    Use login method in login component

--- drinkerClient/src/app/account/login/login.component.ts
+++ drinkerClient/src/app/account/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { AuthUser } from 'src/app/_models/registerUser';
+import { AccountService } from 'src/app/_services/account.service';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +11,28 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor(private modalService: NgbModal) { }
+  constructor(private modalService: NgbModal, private accountService: AccountService, private formBuilder: FormBuilder, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.email],
+      password: ['', Validators.required]
+    })
+  }
+
+  login() {
+    let loginUser = this.loginForm.value as AuthUser;
+    this.accountService.login(loginUser).subscribe(() => {
+      this.toastr.success('Login successfully');
+    }, error => {
+      this.toastr.error(error.error.errors[0]);
+    })
   }
 
   openRegistraionContent(registrationContent: any) {
