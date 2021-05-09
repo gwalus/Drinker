9d49db8a341bf103b38f8ebb0bf29f581f539619
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-09 14:06:23 +0200

    Add register method in account service

--- drinkerClient/src/app/_services/account.service.ts
+++ drinkerClient/src/app/_services/account.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { RegisterUser } from '../_models/registerUser';
 import { User } from '../_models/user';
 
 @Injectable({
@@ -14,6 +16,25 @@ export class AccountService {
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
+
+  register(model: RegisterUser) {
+    let registerUser: RegisterUser = {
+      username: 'user2',
+      password: 'Pa$$w0rd',
+      confirmPassword: 'Pa$$w0rd'
+    };
+
+    model = registerUser;
+
+    return this.http.post(this.baseUrl + 'identity/register', model).pipe(
+      map(response => {
+        const user = response as User;
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    )
+  }
 
   setCurrentUser(user: User) {
     localStorage.setItem('user', JSON.stringify(user));
