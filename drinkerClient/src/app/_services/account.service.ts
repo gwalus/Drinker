e769f1678fed8986f888f9478f59f0cfb5da7f6e
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-09 13:50:04 +0200

    Set current user method in account service

--- drinkerClient/src/app/_services/account.service.ts
+++ drinkerClient/src/app/_services/account.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +10,13 @@ import { environment } from 'src/environments/environment';
 export class AccountService {
   baseUrl = environment.apiUrl;
 
+  private currentUserSource = new ReplaySubject(1);
+  currentUser$ = this.currentUserSource.asObservable();
+
   constructor(private http: HttpClient) { }
+
+  setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUserSource.next(user);
+  }
 }
