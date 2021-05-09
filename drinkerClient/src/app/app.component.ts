645aae2c8ba5ab13f7e33e29dd44ef50551c7e95
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-09 22:14:29 +0200

    Set current user method in app component

--- drinkerClient/src/app/app.component.ts
+++ drinkerClient/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'drinkerClient';
+export class AppComponent implements OnInit {
+  title = 'Drinker';
+
+  constructor(private accountService: AccountService) {
+  }
+
+  ngOnInit(): void {
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      this.accountService.setCurrentUser(user);
+    }
+  }
 }
