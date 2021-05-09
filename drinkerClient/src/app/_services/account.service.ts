6e0ff8e3209bd5a394b1eed90b0e8c5baf10468b
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-09 13:45:11 +0200

    Add registerUser model in client

--- drinkerClient/src/app/_services/account.service.ts
+++ drinkerClient/src/app/_services/account.service.ts
@@ -1,9 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
+  baseUrl = environment.apiUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
