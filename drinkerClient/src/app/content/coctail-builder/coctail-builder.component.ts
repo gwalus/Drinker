ca42f1901ed2696526182f438d878dadf1f2e8d2
Sławomir Kędzior <matikm18@gmail.com>
2021-05-13 23:18:35 +0200

    add Coctail builder dynamic form

--- drinkerClient/src/app/content/coctail-builder/coctail-builder.component.ts
+++ drinkerClient/src/app/content/coctail-builder/coctail-builder.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-coctail-builder',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoctailBuilderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private coctail:FormBuilder) {
+  
+    this.CreateCoctailForm = this.coctail.group({
+      coctailName: '',
+      alcoholic: '',
+      category:'',
+      validatedCustomFile:'',
+      glass: '',
+      instructions: '',
+      ingredients: this.coctail.array([]),
+    });
+    this.addIngredient();
+  }
+
+  CreateCoctailForm: FormGroup;
 
   ngOnInit(): void {
+  }
+
+  ingredients() : FormArray {
+    return this.CreateCoctailForm.get("ingredients") as FormArray
+  }
+   
+  newIngredient(): FormGroup {
+    return this.coctail.group({
+      name: '',
+      measure: '',
+    })
+  }
+   
+  addIngredient() {
+    this.ingredients().push(this.newIngredient());
+  }
+   
+  removeIngredient(i:number) {
+    this.ingredients().removeAt(i);
+  }
+   
+  onSubmit() {
+    console.log(this.CreateCoctailForm.value);
   }
 
 }
