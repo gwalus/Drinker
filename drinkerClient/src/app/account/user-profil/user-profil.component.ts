73180f55b3a049d8517a815467cd32fda81f65f2
Sławomir Kędzior <matikm18@gmail.com>
2021-05-25 16:35:37 +0200

    My accont add user data to view

--- drinkerClient/src/app/account/user-profil/user-profil.component.ts
+++ drinkerClient/src/app/account/user-profil/user-profil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'src/app/_services/account.service';
 
 @Component({
   selector: 'app-user-profil',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfilComponent implements OnInit {
 
-  constructor() { }
+  constructor(public accountService: AccountService) { }
 
   ngOnInit(): void {
   }
