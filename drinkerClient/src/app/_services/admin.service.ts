5c36ad67652e476645abbe59baf99a6c572e1ffb
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-25 12:27:30 +0200

    Add get cocktails to accept method in admin service

--- drinkerClient/src/app/_services/admin.service.ts
+++ drinkerClient/src/app/_services/admin.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { getPaginatedResult, getPaginationHeaders } from '../_helpers/paginationHelper';
+import { Coctail } from '../_models/coctail';
+import { PaginationParams } from '../_models/paginationParams';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminService {
+  baseUrl = environment.apiUrl + 'admin/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCocktailsToAccept(paginationParams: PaginationParams) {
+    let params = new HttpParams();
+
+    params = getPaginationHeaders(params, paginationParams);
+
+    return getPaginatedResult<Coctail[]>(this.baseUrl + 'for-approval', params, this.http);
+  }
 }
