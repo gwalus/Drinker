cd51632eafd5ccac0bd07db93928aca3bc50175b
Sławomir Kędzior <matikm18@gmail.com>
2021-05-28 01:30:55 +0200

    Create Home page

--- drinkerClient/src/app/home/home.component.ts
+++ drinkerClient/src/app/home/home.component.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Coctail } from '../_models/coctail';
 import { AccountService } from '../_services/account.service';
+import { CoctailService } from '../_services/coctail.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  constructor(public accountService: AccountService) { }
+  carusele: Coctail[];
+  randomCoctails: Coctail[];
+
+  constructor(public accountService: AccountService, private coctailService: CoctailService) { }
 
   ngOnInit(): void {
+    this.getRandomCoctails();
   }
 
   logout() {
     this.accountService.logout();
+  }
+
+  getRandomCoctails(){
+    this.coctailService.getRandomCoctails(12).subscribe(coctails => {
+      if(coctails){
+        this.randomCoctails = coctails;
+        this.carusele = this.randomCoctails.filter((u, i) => i < 3);
+      }
+    })
   }
 
 }
