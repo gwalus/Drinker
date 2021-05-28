93bbab14286e33a5f32759f3e6d16a7050f99d1b
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-28 19:04:07 +0200

    Get username method for claims extensions

---
+++ DrinkerAPI/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace DrinkerAPI.Extensions
+{
+    public static class ClaimsExtensions
+    {
+        public static string GetUserName(this ClaimsPrincipal user)
+        {
+            return user.FindFirst(ClaimTypes.Name)?.Value;
+        }
+    }
+}
