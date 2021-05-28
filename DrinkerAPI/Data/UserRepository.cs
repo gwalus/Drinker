163a12612d9917fd17aa1d2ecfb4ae5f8c6d3a27
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-28 22:32:22 +0200

    Get app user method in user repository

---
+++ DrinkerAPI/Data/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿using DrinkerAPI.Interfaces;
+using DrinkerAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace DrinkerAPI.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly CoctailContext _coctailContext;
+
+        public UserRepository(CoctailContext coctailContext)
+        {
+            _coctailContext = coctailContext;
+        }
+
+        public async Task<AppUser> GetUserById(int id)
+        {
+            return await _coctailContext.Users.SingleOrDefaultAsync(u => u.Id == id);
+        }
+    }
+}
