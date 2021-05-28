1987dec523bac29da2bba3823b59085e7a0ab27f
Sławomir Kędzior <matikm18@gmail.com>
2021-05-28 16:37:59 +0200

    add favorite dirnk style

--- drinkerClient/src/app/account/favorite-drink/favorite-drink.component.ts
+++ drinkerClient/src/app/account/favorite-drink/favorite-drink.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Coctail } from 'src/app/_models/coctail';
+import { CoctailParams } from 'src/app/_models/coctailParams';
+import { PaginationParams } from 'src/app/_models/paginationParams';
+import { CoctailService } from 'src/app/_services/coctail.service';
 
 @Component({
   selector: 'app-favorite-drink',
   templateUrl: './favorite-drink.component.html',
-  styleUrls: ['./favorite-drink.component.css']
+  styleUrls: ['./favorite-drink.component.scss', '../style.css']
 })
 export class FavoriteDrinkComponent implements OnInit {
 
-  constructor() { }
+  constructor(private coctailService: CoctailService) { }
 
   ngOnInit(): void {
+    this.getCoctails();
+  }
+
+  coctailParams: CoctailParams = new CoctailParams();
+  paginationParams: PaginationParams = new PaginationParams();
+  viewCoctails: Coctail[];
+  
+  getCoctails() {
+    this.coctailService.getAll('', [], this.coctailParams).subscribe(coctails => {
+      if(coctails.result.length>0)
+        this.viewCoctails = coctails.result;
+    })
   }
 
 }
