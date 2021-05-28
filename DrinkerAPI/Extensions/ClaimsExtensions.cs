dddb3084e9716f77dd568bfd743ed5aefc54b759
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-28 19:04:27 +0200

    Get user by id method for claims extensions

--- DrinkerAPI/Extensions/ClaimsExtensions.cs
+++ DrinkerAPI/Extensions/ClaimsExtensions.cs
@@ -8,5 +8,10 @@ namespace DrinkerAPI.Extensions
         {
             return user.FindFirst(ClaimTypes.Name)?.Value;
         }
+
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+        }
     }
 }
