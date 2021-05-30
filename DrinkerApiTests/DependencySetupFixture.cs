fd74848049e365f5595c213b9ec44ddbec6421e8
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-30 17:17:47 +0200

    DependencySetupFixture for unit tests

---
+++ DrinkerApiTests/DependencySetupFixture.cs
@@ -0,0 +1,27 @@
+﻿using DrinkerAPI.Data;
+using DrinkerAPI.Helpers;
+using DrinkerAPI.Interfaces;
+using DrinkerAPI.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace DrinkerApiTests
+{
+    public class DependencySetupFixture
+    {
+        public DependencySetupFixture()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<CoctailContext>(options => options.UseInMemoryDatabase(Guid.NewGuid().ToString()));
+
+            serviceCollection.AddScoped<ICoctailRepository, CoctailRepository>();
+            serviceCollection.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+            serviceCollection.AddScoped<ICloudinaryService, CloudinaryService>();
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        public ServiceProvider ServiceProvider { get; }
+    }
+}
