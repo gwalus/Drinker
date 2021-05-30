72e46533362cf8da7b26c0cb36c5952a068135df
Grzegorz Waluś <gwalus1404@gmail.com>
2021-05-30 13:23:03 +0200

    Add busy method in busy service

--- drinkerClient/src/app/_services/busy.service.ts
+++ drinkerClient/src/app/_services/busy.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BusyService {
+  busyRequestCount = 0;
 
-  constructor() { }
+  constructor(private spinnerService: NgxSpinnerService) { }
+
+  busy() {
+    this.busyRequestCount++;
+    this.spinnerService.show(undefined, {
+      type: 'line-scale-party',
+      bdColor: 'rgba(255,255,255,0)',
+      color: '#333333'
+    });
+  }
 }
